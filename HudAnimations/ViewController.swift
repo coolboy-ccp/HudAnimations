0dc9fd298a14e9c6732246354963e709736a0253
储诚鹏 <chuchengpeng@chuchengpengdemacbook-pro.local>
2018-04-02 15:06:32 +0800

    Initial Commit

---
+++ HudAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HudAnimations
+//
+//  Created by 储诚鹏 on 2018/4/2.
+//  Copyright © 2018年 储诚鹏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
